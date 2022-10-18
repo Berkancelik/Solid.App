b9d841e64e9a742c3dc773943c8bcabe8a3c0269
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-10-18 23:58:34 +0300

    Bad code created

---
+++ Solid.App/SingleResponsibiltyPrnciple.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.App.SingleResponsibiltyPrnciple
+{
+    
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private static List<Product> products = new List<Product>();
+
+        public Product()
+        {
+          
+        }
+    }
+}
